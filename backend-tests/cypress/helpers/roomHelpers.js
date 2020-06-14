d2224525d830d2a38d916d9944951f4ed1b56752
Henrik Gustafsson <henrik.gustafsson@consid.se>
2020-06-14 19:33:16 +0200

    added fakers to payload

--- backend-tests/cypress/helpers/roomHelpers.js
+++ backend-tests/cypress/helpers/roomHelpers.js
@@ -4,83 +4,88 @@ const ENDPOINT_CREATE_ROOM = 'http://localhost:3000/api/room/new'
 const ENDPOINT_GET_ROOMS = 'http://localhost:3000/api/rooms'
 const ENDPOINT_GET_ROOM = 'http://localhost:3000/api/room/'
 
-function createRandomRoomPayload(){
-    // const fakeRoomNumber = faker.number()
-    // const fakeFloorNumber = faker.internet.email()
-    // const fakeRoomPrice = faker.phone.phoneNumber()
+// Create partially random payload for the rooms
+function createRandomRoomPayload() {
+    const fakeRoomNumber = faker.random.number({min:101, max:499})
+    const fakeFloorNumber = faker.random.number({min:2, max:5})
+    const fakeRoomPrice = faker.random.number({min:500, max:1499})
     const payload = {
-        "features":["balcony","sea_view"],
-        "category":"twin",
-        "number":"345",
-        "floor":"3",
-        "available":true,
-        "price":"950"
+        "features": ["balcony", "sea_view"],
+        "category": "twin",
+        "number": fakeRoomNumber,
+        "floor": fakeFloorNumber,
+        "available": true,
+        "price": fakeRoomPrice
     }
     return payload
 }
 
-function getRooms(cy){
-    cy.authenticateSession().then((response =>{
-    cy.request({
-    method: "GET",
-    url: ENDPOINT_GET_ROOMS,
-    headers:{
-            'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
-            'Content-Type': 'application/json'
-    },
-}).then((response =>{
-    const responseAsString = JSON.stringify(response)
-    cy.log(responseAsString)
-}))
+// Function get rooms. Lists all rooms 
+function getRooms(cy) {
+    cy.authenticateSession().then((response => {
+        cy.request({
+            method: "GET",
+            url: ENDPOINT_GET_ROOMS,
+            headers: {
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+        }).then((response => {
+            const responseAsString = JSON.stringify(response)
+            cy.log(responseAsString)
+        }))
     }))
 }
 
-function createRoom(){
-    cy.authenticateSession().then((response =>{
+// Function create room. Creates a new room using the randomly generated payload
+function createRoom() {
+    cy.authenticateSession().then((response => {
         let roomPayload = createRandomRoomPayload()
 
         // Post request to create a room
         cy.request({
             method: "POST",
             url: ENDPOINT_CREATE_ROOM,
-            headers:{
+            headers: {
                 'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
                 'Content-Type': 'application/json'
-        },
-        body:roomPayload
-    }).then((response =>{
-        const responseAsString = JSON.stringify(response)
-        expect(responseAsString).to.have.string(roomPayload.number)
+            },
+            body: roomPayload
+        }).then((response => {
+            const responseAsString = JSON.stringify(response)
+            expect(responseAsString).to.have.string(roomPayload.number)
+            expect(responseAsString).to.have.string(roomPayload.price)
+
+        }))
+
+        //getClientsWithAssertion(cy, fakeClientPayload.name, fakeClientPayload.email, fakeClientPayload.telephone)
+
     }))
-
-    //getClientsWithAssertion(cy, fakeClientPayload.name, fakeClientPayload.email, fakeClientPayload.telephone)
-
-}))
 }
 
+// Function to delete the last generated room
+function deleteLastCreatedRoomAfterGet(cy) {
+    cy.authenticateSession().then((response => {
 
-function deleteLastCreatedRoomAfterGet(cy){
-    cy.authenticateSession().then((response =>{
-
-    cy.request({
-    method: "GET",
-    url: ENDPOINT_GET_ROOMS,
-    headers:{
-            'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
-            'Content-Type': 'application/json'
-    },
-}).then((response =>{
-    let lastId = response.body[response.body.length -1].id
-    cy.request({
-        method: "DELETE",
-        url: ENDPOINT_GET_ROOM+lastId,
-        headers:{
-            'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
-            'Content-Type': 'application/json'
-    },
-    })
-}))
-}))
+        cy.request({
+            method: "GET",
+            url: ENDPOINT_GET_ROOMS,
+            headers: {
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+        }).then((response => {
+            let lastId = response.body[response.body.length - 1].id
+            cy.request({
+                method: "DELETE",
+                url: ENDPOINT_GET_ROOM + lastId,
+                headers: {
+                    'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                    'Content-Type': 'application/json'
+                },
+            })
+        }))
+    }))
 
 }
 
--- backend-tests/cypress/integration/backend-auth.spec.js
+++ backend-tests/cypress/integration/backend-auth.spec.js
@@ -2,57 +2,63 @@ import * as clientHelpers from '../helpers/clientHelpers'
 import * as roomHelpers from '../helpers/roomHelpers'
 
 
-describe('testing auth', function(){
+describe('testing auth', function () {
 
-    it('test case 1', function(){
-        cy.authenticateSession().then((response =>{
+    // First test case. Not using helpers
+    it('test case 1, not using helpers', function () {
+        cy.authenticateSession().then((response => {
             cy.request({
                 method: "GET",
                 url: 'http://localhost:3000/api/clients',
-                headers:{
+                headers: {
                     'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
                     'Content-Type': 'application/json'
-            },
-        }).then((response =>{
-            cy.log(response.body[0].id)
-            cy.log(response.body[0].created)
-            cy.log(response.body[0].name)
-            cy.log(response.body[0].email)
-            cy.log(response.body[0].telephone)
-            cy.logoutuser()
+                },
+            }).then((response => {
+                cy.log(response.body[0].id)
+                cy.log(response.body[0].created)
+                cy.log(response.body[0].name)
+                cy.log(response.body[0].email)
+                cy.log(response.body[0].telephone)
+                cy.logoutuser()
+            }))
         }))
-    }))
     })
 
-
-    it('Create a new client', function(){
+    // Test case. Create a new client 
+    it('Create a new client', function () {
         clientHelpers.createClient(cy),
-        cy.logoutuser()
+            cy.logoutuser()
     })
 
-    it('Get clients', function(){
+    // Test case. List all clients
+    it('Get clients', function () {
         clientHelpers.getClients(cy),
-        cy.logoutuser()
+            cy.logoutuser()
     })
 
-    it('Create client and delete', function(){
+    // Test case. Create a client and delete it
+    it('Create client and delete', function () {
         clientHelpers.createClientRequestAndDelete(cy),
-        cy.logoutuser()
+            cy.logoutuser()
     })
 
-    it('Get rooms', function(){
+    // Test case. List all rooms
+    it('Get rooms', function () {
         roomHelpers.getRooms(cy),
-        cy.logoutuser()
+            cy.logoutuser()
     })
 
-    it('Create a new room', function(){
+    // Test case. Create a new room
+    it('Create a new room', function () {
         roomHelpers.createRoom(cy),
-        cy.logoutuser()
+            cy.logoutuser()
     })
 
-    it.only('Delete last created room after get', function(){
+    // Test case. Create a room and delete it.
+    it('Delete last created room after get', function () {
         roomHelpers.createRoom(cy),
-        roomHelpers.deleteLastCreatedRoomAfterGet(cy),
-        cy.logoutuser()
+            roomHelpers.deleteLastCreatedRoomAfterGet(cy),
+            cy.logoutuser()
     })
 })
